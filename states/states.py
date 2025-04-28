cd50f08e53f632f698fead1f82678e4378f3244f
Luzanow <Tempsupor@gmail.com>
2025-04-28 22:18:17 +0300

    Update states.py

--- states/states.py
+++ states/states.py
@@ -3,5 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 class ProfileStates(StatesGroup):
     name = State()
     age = State()
+    gender = State()
+    city = State()
     bio = State()
     photo = State()
