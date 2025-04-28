cf7bd437d4a361f8bc1aca57bcdf798e2431dc7a
Luzanow <Tempsupor@gmail.com>
2025-04-28 08:21:37 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,9 +1,23 @@
-import aiosqlite
+from database.db import get_db
 
 async def create_profile(telegram_id, name, age, gender, bio, photo, looking_for):
-    async with aiosqlite.connect("your_database.db") as db:
-        await db.execute('''
-            INSERT INTO profiles (telegram_id, name, age, gender, bio, photo, looking_for)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (telegram_id, name, age, gender, bio, photo, looking_for))
-        await db.commit()
+    db = await get_db()
+    await db.execute('''
+        INSERT INTO profiles (telegram_id, name, age, gender, bio, photo, looking_for)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (telegram_id, name, age, gender, bio, photo, looking_for))
+    await db.commit()
+    await db.close()
+
+async def get_random_user(current_user_id):
+    db = await get_db()
+    cursor = await db.execute('''
+        SELECT id, telegram_id, name, age, gender, bio, photo, looking_for
+        FROM profiles
+        WHERE telegram_id != ?
+        ORDER BY RANDOM()
+        LIMIT 1
+    ''', (current_user_id,))
+    user = await cursor.fetchone()
+    await db.close()
+    return [user] if user else []
