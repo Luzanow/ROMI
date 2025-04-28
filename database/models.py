27047ca4810ccdd57748e5eda88c6d19412a0ae8
Luzanow <Tempsupor@gmail.com>
2025-04-28 21:49:17 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,6 +1,7 @@
 import aiosqlite
 from config import DB_PATH
 
+# Створення анкети
 async def create_profile(telegram_id: int, name: str, age: int, bio: str, gender: str, looking_for: str, photo: str):
     async with aiosqlite.connect(DB_PATH) as db:
         await db.execute(
@@ -8,4 +9,22 @@ async def create_profile(telegram_id: int, name: str, age: int, bio: str, gender
             (telegram_id, name, age, bio, gender, looking_for, photo)
         )
         await db.commit()
-        
+
+# Отримати свою анкету
+async def get_profile(telegram_id: int):
+    async with aiosqlite.connect(DB_PATH) as db:
+        cursor = await db.execute(
+            "SELECT name, age, bio, gender, looking_for, photo FROM users WHERE telegram_id = ?",
+            (telegram_id,)
+        )
+        profile = await cursor.fetchone()
+        return profile
+
+# Оновити анкету
+async def update_profile(telegram_id: int, name: str, age: int, bio: str, gender: str, looking_for: str, photo: str):
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute(
+            "UPDATE users SET name = ?, age = ?, bio = ?, gender = ?, looking_for = ?, photo = ? WHERE telegram_id = ?",
+            (name, age, bio, gender, looking_for, photo, telegram_id)
+        )
+        await db.commit()
